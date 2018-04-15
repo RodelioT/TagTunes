12b7f69de6ababec98a83850ee95a7dcd7fa4f2f
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2018-04-15 17:21:07 -0500

    Buttons now appearing in the Appbar
    Including the previously implemented shuffle and close buttons.
    Planning on removing those, or possibly reimplementing them.

--- app/src/main/java/ca/google/TagTunes/MainActivity.java
+++ app/src/main/java/ca/google/TagTunes/MainActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -121,6 +122,12 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
